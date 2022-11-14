3db5a557181c66de4cce5bf3846808e5ca76a45d
sasha <alextemchur7@gmail.com>
2022-11-14 22:12:25 +0200

    All okey and logs

--- store/dborm.py
+++ store/dborm.py
@@ -1,18 +1,25 @@
+from loguru import logger
 import django, os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 django.setup()
 from django.core.management.base import BaseCommand
 from bot.models import InfoProducts
 
+logger.add("bot/bot_logs/bot.log", format="{time} {level} {message}", rotation="10MB", compression="zip")
 
+
+@logger.catch
 def delete_product():
     
+    logger.info("The (delete_product) function worked")
     product = InfoProducts.objects.all()
     product.filter(status=0).delete()
     
     
+@logger.catch
 def get_products_id():
     
+    logger.info("The (get_products_id) function worked")
     list_id = []
     products = InfoProducts.objects.filter(status=True)
     products = products.values_list('id', flat=True)
@@ -20,8 +27,11 @@ def get_products_id():
         list_id.append(products[i])
     return list_id
 
+
+@logger.catch
 def name(id):
     
+    logger.info("The (name) function worked")
     list_product_name  = []
     for i in range(len(get_products_id())):
         product = InfoProducts.objects.get(id=id[i])
@@ -30,8 +40,10 @@ def name(id):
     return list_product_name
 
 
+@logger.catch
 def description_image_name_price(id):
     
+    logger.info("The (description_image_name_price) function worked")
     description_image_name_price  = []
     for i in range(len(get_products_id())):
         product = InfoProducts.objects.get(id=id[i])
@@ -43,7 +55,10 @@ def description_image_name_price(id):
     return description_image_name_price
 
 
+@logger.catch
 def count_id():
     
+    logger.info("The (count_id) function worked")
     product = InfoProducts.objects.filter(status=True)
     return len(product)
+
--- store/telegrambot.py
+++ store/telegrambot.py
@@ -1,53 +1,63 @@
 from telebot.async_telebot import AsyncTeleBot
 from telebot import types
-import django, os, asyncio, math
+import django, os, asyncio, math, time
+from loguru import logger
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 django.setup()
 from django.core.management.base import BaseCommand
 from dborm import *
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
-
+logger.add("bot/bot_logs/bot.log", format="{time} {level} {message}", rotation="10MB", compression="zip")
 bot = AsyncTeleBot('5619487724:AAFeBptlX1aJ9IEAFLMUXN3JZBImJ35quWk')   
 product = name(get_products_id())
 pages = math.ceil(count_id()/10)
 page = 1
 first_product = 0
 last_product = 10
-editing_message = []
+audit = []
+index_list = []
 
 
 @bot.message_handler(commands=['start']) 
+@logger.catch
 async def start(message):
     
     delete_product()
     markut_products = types.ReplyKeyboardMarkup(resize_keyboard=True)
     products = types.KeyboardButton('Products')
     markut_products.add(products)
+    audit.append([str(message.chat.id)])
+    index_list.append(str(message.chat.id))
+    index_list.append(int(len(audit)-1))                 
     await bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}', reply_markup=markut_products)
+    logger.info("The (start) function worked")
     
-
-
+ 
 @bot.message_handler(content_types=['text'])
+@logger.catch
 async def text(message):
 
     delete_product()
-    if message.text == 'Products' and "status" not in editing_message:
+    if message.text == 'Products' and "status" not in audit[index_list[index_list.index(str(message.chat.id)) + 1]] and audit[index_list[index_list.index(str(message.chat.id)) + 1]][0] == str(message.chat.id):
         list_products = types.InlineKeyboardMarkup(row_width=5)
         for i in range(0, 10):
             list_products.add(types.InlineKeyboardButton(text=f'{product[i]}', callback_data=f'{product[i]}'))
         list_products.add(types.InlineKeyboardButton(text=f'<< 1', callback_data='first_page'),types.InlineKeyboardButton(text=f'<', callback_data='next_page'),
                             types.InlineKeyboardButton(text=f'{page}/{pages}', callback_data=' '),
                             types.InlineKeyboardButton(text=f'>', callback_data='back_page'),types.InlineKeyboardButton(text=f'{pages} >>', callback_data='last_page'))
-        editing_message.append("status")
+        audit[index_list[index_list.index(str(message.chat.id)) + 1]].append("status")
         await bot.send_message(message.chat.id, f"Products page {page}", reply_markup=list_products)
     
     elif message.text:
         await bot.delete_message(message.chat.id, message.id)
-    
+    logger.info("The (text) function worked")
+     
          
 @bot.callback_query_handler(func=lambda call: True)
+@logger.catch
 async def callback_worker(call):
+    
     global page, pages, first_product, last_product
     for a in range(len(product)):
         if call.data == product[a]:
@@ -57,14 +67,11 @@ async def callback_worker(call):
                     markut_back = types.InlineKeyboardMarkup()
                     back = types.InlineKeyboardButton('Back', callback_data="back")
                     markut_back.add(back)
-                    await bot.send_photo(call.message.chat.id, open(description_image_change[i][0], 'rb'))
-                    await bot.send_message(call.message.chat.id, f"{description_image_change[i][1]}\n{description_image_change[i][2]}\n{description_image_change[i][3]}", reply_markup=markut_back)
-    
+                    await bot.send_photo(call.message.chat.id, open(description_image_change[i][0], 'rb'), caption=f"{description_image_change[i][1]}\n{description_image_change[i][2]}\n{description_image_change[i][3]}", reply_markup=markut_back)
+
     
     if call.data == "back":
-        await bot.delete_message(call.message.chat.id, call.message.id)
-        await bot.delete_message(call.message.chat.id, call.message.id-1)
-        
+        await bot.delete_message(call.message.chat.id, call.message.id) 
         
     elif call.data == "first_page" and page != 1:
         page = 1    
@@ -78,7 +85,6 @@ async def callback_worker(call):
     
     elif call.data == "next_page" and page != pages:
         if page < pages:
-            
             page = page+1
             if page == 2:
                 first_product = 10
@@ -94,10 +100,8 @@ async def callback_worker(call):
             
             elif page == pages:
                 count_last_prod = str(count_id()/10)
-                print(count_last_prod)
                 index = count_last_prod.find(".")
                 count_last_prod = count_last_prod[index+1:]
-                print(count_last_prod)
                 if count_last_prod == '0':
                     count_last_prod = 10
                 list_products = types.InlineKeyboardMarkup(row_width=5)
@@ -111,9 +115,6 @@ async def callback_worker(call):
             else:
                 first_product = first_product+10
                 last_product = last_product+10
-            
-                
-                print("a = ", page, first_product, last_product)
                 list_products = types.InlineKeyboardMarkup(row_width=5)
                 for i in range(first_product, last_product):
 
@@ -147,7 +148,6 @@ async def callback_worker(call):
             else:
                 first_product = first_product-10
                 last_product = last_product-10
-                print('b = ', page, first_product, last_product)
                 list_products = types.InlineKeyboardMarkup(row_width=5)
                 for i in range(first_product, last_product):   
                     list_products.add(types.InlineKeyboardButton(text=f'{product[i]}', callback_data=f'{product[i]}'))
@@ -159,10 +159,8 @@ async def callback_worker(call):
     elif call.data == "last_page" and page != pages:
         page = pages   
         count_last_prod = str(count_id()/10)
-        print(count_last_prod)
         index = count_last_prod.find(".")
         count_last_prod = count_last_prod[index+1:]
-        print(count_last_prod)
         if count_last_prod == '0':
             count_last_prod = 10
         list_products = types.InlineKeyboardMarkup(row_width=5)
@@ -172,4 +170,16 @@ async def callback_worker(call):
                             types.InlineKeyboardButton(text=f'{page}/{pages}', callback_data=' '),
                             types.InlineKeyboardButton(text=f'>', callback_data='next_page'),types.InlineKeyboardButton(text=f'{pages} >>', callback_data='last_page'))
         await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Products page {page}", reply_markup=list_products)
-asyncio.run(bot.polling()) 
+    logger.info("The (callback_worker) function worked")
+
+
+while True:
+    try:
+        logger.info("Start bot")
+        asyncio.run(bot.polling(non_stop=True, interval=1, timeout=0))
+    except KeyboardInterrupt:
+        logger.info("Stopping bot")
+        exit()
+    except Exception:
+        logger.info("Error")
+        time.sleep(5)
